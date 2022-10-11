efe204e482e074d1ab4f3f30becd25079d7bb511
Joseph Wick <josephwick@outlook.com>
2022-10-11 11:14:32 -0700

    Update BP1_visco_hm.m

--- BP1_visco_hm.m
+++ BP1_visco_hm.m
@@ -375,7 +375,8 @@ function out = run(b)
         sterm12 = e12Terms(x2p, x3p, m, n, w);
         sterm13 = e13Terms(x2p, x3p, m, n, w);
       end
-      ss.e12p_plate = ss.Vpl * (1/2*w + 1/w)*(summ12);
+      ss.e12p_plate(i,j) = ss.Vpl * (1/2*w + 1/w)*(summ12);
+      ss.e13_plate(i,j) = ss.Vpl * (-1/(w*n^0.5)*(summ13) );
 
     end
   end
