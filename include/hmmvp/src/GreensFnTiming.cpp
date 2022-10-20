7f220beb3f13dd358d5d0d4fa6198bf891119b52
Joseph Wick <josephwick@outlook.com>
2022-10-20 15:46:35 -0700

    Update GreensFnTiming.cpp

--- include/hmmvp/src/GreensFnTiming.cpp
+++ include/hmmvp/src/GreensFnTiming.cpp
@@ -3,7 +3,7 @@
 class GreensFnTiming : public ImplGreensFn {
 public:
   virtual void Init(const KeyValueFile* kvf) throw (Exception);
-  virtual Hd* ComputeHd (double eta) { return NewHd(_k, _k, NULL, eta); }
+  virtual Hd* ComputeHd (double eta) { return NewHd(_x, _x, NULL, eta); }
   virtual bool Call(const CompressBlockInfo& cbi, const vector<UInt>& rs,
                     const vector<UInt>& cs, double* B) const;
 
@@ -11,6 +11,9 @@ private:
 
   // kernel
   Matd _k;
+
+  // for sizing only
+  Matd _x;
 
   double Eval(UInt i, UInt j) const;
 };
@@ -27,10 +30,13 @@ inline double GreensFnTiming::Eval (UInt i, UInt j) const {
 void GreensFnTiming::Init (const KeyValueFile* kvf) throw (Exception) {
   const Matd* m;
   const Matd* n;
-  const Matd* o;
 
   const Matd* l;
   const Matd* w;
+
+  if (!kvf->GetMatd("X", m)) throw exception("Missing X.");
+  _x = *m;
+  if (_x.Size(1) != 3) throw Exception("X must be 3xN.");
 
   if (!kvf->GetMatd("K", n)) throw Exception("Missing K.");
   _k = *n;
