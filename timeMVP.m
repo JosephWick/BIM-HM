38a25aceecb099bd50bb460cdcd4237dd8fd377e
Joseph Wick <josephwick@outlook.com>
2022-10-20 14:38:45 -0700

    Update timeMVP.m

--- timeMVP.m
+++ timeMVP.m
@@ -76,7 +76,8 @@ function r = build()
 
     % tolerance 1e-6;
     c.tol = 1e-6;
-    c.write_hmat_filename = strcat('./tmp/t3d_n',nstring,'_e6');
+    %c.write_hmat_filename = strcat('./tmp/t3d_n',nstring,'_e6');
+    c.write_hmat_filename = 'a';
     c.write_hd_filename = strcat(c.write_hmat_filename,'-hd');
     c.kvf = strcat(c.write_hmat_filename, '.kvf');
     kvf('Write', c.kvf, c, 4);
