071a7bd31a99c05bbe233cfd1c5a06f4795beb40
Joseph Wick <josephwick@outlook.com>
2022-10-26 13:16:33 -0700

    Update BP1_visco_hm.m

--- BP1_visco_hm.m
+++ BP1_visco_hm.m
@@ -393,8 +393,8 @@ function out = run(b)
   ss.e13p_plate = ss.e13p_plate';
   ss.e12p_plate = ss.e12p_plate(:);
   ss.e13p_plate = ss.e13p_plate(:);
-  %ss.e12p_plate = 1e-14*ones(length(ss.shearY_chat)*length(ss.shearZ_chat),1);
-  %ss.e13p_plate =      zeros(length(ss.shearY_chat)*length(ss.shearZ_chat),1);
+  ss.e12p_plate = 1e-14*ones(length(ss.shearY_chat)*length(ss.shearZ_chat),1);
+  ss.e13p_plate =      zeros(length(ss.shearY_chat)*length(ss.shearZ_chat),1);
 
   % Rheological Parameters
   % Reference Strain Rate (for stress in MPa)
