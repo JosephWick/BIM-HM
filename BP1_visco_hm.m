be6dca967e765803a4c7952b7b7339164bf728b3
Joseph Wick <josephwick@outlook.com>
2022-11-07 19:22:38 -0800

    Update BP1_visco_hm.m

--- BP1_visco_hm.m
+++ BP1_visco_hm.m
@@ -399,7 +399,7 @@ function out = run(b)
   s120 = (ss.e12p_plate./e_mag_plate).*tau0_mag;
   s130 = (ss.e13p_plate./e_mag_plate).*tau0_mag;
 
-  s120
+  s120;
 
   %s120 = (ss.e12p_plate./ss.A).^(1./ss.n);
   %s130 = (ss.e13p_plate./ss.A).^(1./ss.n);
