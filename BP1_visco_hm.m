e21e4c8e2baad8ba02c20c551fc75a29e896638c
Joseph Wick <josephwick@outlook.com>
2022-11-08 16:13:54 -0800

    Update BP1_visco_hm.m

--- BP1_visco_hm.m
+++ BP1_visco_hm.m
@@ -404,6 +404,7 @@ function out = run(b)
   e120 = zeros(size(s120));
   e130 = zeros(size(s130));
 
+  csvwrite('e12p.csv', ss.e12p_plate);
   csvwrite('s120.csv', s120);
   csvwrite('s130.csv', s130);
 
