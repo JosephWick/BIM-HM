9d253e9b96c2dd34a7817b65423d6bfefbb14955
Joseph Wick <josephwick@outlook.com>
2023-01-03 10:26:29 -0800

    Update stressVertShear_s12.h

--- include/hmmvp/src/stressVertShear_s12.h
+++ include/hmmvp/src/stressVertShear_s12.h
@@ -4,9 +4,31 @@
  */
 
 // main function
-double stressVertShear_s12(double x1, double x2, double x3,
+double s12::stressVertShear_s12(double x1, double x2, double x3,
                            double q1, double q2, double q3,
                            double L, double T, double W, double theta,
                            double epsv11p, double epsv12p, double epsv13p,
                            double epsv22p, double epsv23p, double epsv33p,
                            double G, double nu);
+
+// helpers
+
+// Green's functions
+double s12::r1(double x1, double x2, double x3, double y1, double y2, double y3);
+double s12::r2(double x1, double x2, double x3, double y1, double y2, double y3);
+
+double s12::IU1d2(double y1,double y2,double y3, double lambda,double epsvkk,double G,
+  double epsv11p,double epsv12p,double epsv13p,double epsv22p,double epsv23p,double epsv33p);
+double s12::IU2d1(double y1,double y2, double y3, double lambda, double epsvkk, double G,
+  double epsv11p,double epsv12p,double epsv13p,double epsv22p,double epsv23p,double epsv33p);
+
+// remove inelastic eigenstrain
+int s12::heaviside(double x);
+double s12::Omega(double x);
+double s12::S(double x);
+
+//
+double s12::J1112d2(double y1,double y2,double y3, double nu,double G,
+      double x1,double x2,doube x3);
+double s12::J1113d2(double y1,double y2,double y3, double nu,double G,
+    double x1,double x2,double x3);
