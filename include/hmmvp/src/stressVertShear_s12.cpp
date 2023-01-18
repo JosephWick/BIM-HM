6660d8986e4a3aa634260bdc16d3ccd92e8324c7
Joseph Wick <josephwick@outlook.com>
2023-01-17 20:11:09 -0800

    Update stressVertShear_s12.cpp

--- include/hmmvp/src/stressVertShear_s12.cpp
+++ include/hmmvp/src/stressVertShear_s12.cpp
@@ -358,7 +358,7 @@ double s12::J2123d1(double y1,double y2,double y3, double nu,double G,
       x1-y1,2)+pow(x2-y2,2))*y3-(lr2-3*x3)*pow(y3,2)+
       pow(y3,3))+y3*(pow(x1-y1,2)+pow(x2-y2,2)-(lr2-x3+(
       -1)*y3)*(2*x3+y3)))+4*pow(lr2,-1)*pow(x1-y1,2)*pow(pow(x1-
-      y1,2)+pow(x2-y2,2)-2)*((-3)*nu*(x3+y3)*(pow(x1-y1,
+      y1,2)+pow(x2-y2,2),-2)*((-3)*nu*(x3+y3)*(pow(x1-y1,
       2)+pow(x2-y2,2)+pow(x3+y3,2))+2*pow(nu,2)*(x3+y3)*(pow(x1-y1,
       2)+pow(x2-y2,2)+pow(x3+y3,2))+y3*(pow(x1-y1,2)+pow(x2-
       y2,2)+(x3+y3)*(2*x3+y3)))+pow(lr2,-1)*pow(pow(x1-y1,2)+pow(x2+(-1)
