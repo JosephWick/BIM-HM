6aa67e3c815967a5628d7de905ea4df558e22108
Joseph Wick <josephwick@outlook.com>
2023-01-17 19:44:21 -0800

    Update stressVertShear_s12.cpp

--- include/hmmvp/src/stressVertShear_s12.cpp
+++ include/hmmvp/src/stressVertShear_s12.cpp
@@ -579,8 +579,8 @@ double s12::J2213d1(double y1,double y2,double y3, double nu,double G,
       2)+pow(x3+y3,2))-y3*(pow(x2-y2,2)+(x3+y3)*(2*x3+y3)))+
       pow(x1,2)*((-3)*nu*(x3+y3)*(pow(x2-y2,2)+pow(x3+y3,2))+2*
       pow(nu,2)*(x3+y3)*(pow(x2-y2,2)+pow(x3+y3,2))+y3*(pow(x2-y2,
-      2)+(x3+y3)*(2*x3+y3))))+2*pow(lr2,-1)*(pow(x1-y1,2)+pow(x2+(-1)
-      *y2),2),-1)*pow(pow(x2-y2,2)+pow(x3+y3,2),-1)*(-x3*
+      2)+(x3+y3)*(2*x3+y3))))+2*pow(lr2,-1)*pow(pow(x1-y1,2)+pow(x2+(-1)
+      *y2,2),-1)*pow(pow(x2-y2,2)+pow(x3+y3,2),-1)*(-x3*
       pow(y1,2)*pow(y2,2)+2*pow(x3,4)*y3+6*pow(x3,2)*pow(y1,2)*y3+4*pow(x3,2)*pow(y2,2)*y3+
       2*pow(y1,2)*pow(y2,2)*y3+pow(y2,4)*y3+7*pow(x3,3)*pow(y3,2)+9*x3*pow(y1,2)*pow(y3,2)+
       6*x3*pow(y2,2)*pow(y3,2)+9*pow(x3,2)*pow(y3,3)+3*pow(y1,2)*pow(y3,3)+2*pow(y2,2)*
