573606ca3bf44abdbbdb7d4ed613521e4e9e76ac
Joseph Wick <josephwick@outlook.com>
2023-02-20 11:13:55 -0800

    Create getE12.m

---
+++ include/getE12.m
@@ -0,0 +1,13 @@
+% terms in the summation for e13
+function y = getE12(x2p, x3p, n, w)
+  m=1;
+  sum = 0.0;
+
+  sterm = e12Terms(x2p,x3p, m, n, w);
+  while sterm >= sum*0.001
+    sum = sum + sterm;
+    m = m+1;
+    sterm = e12Terms(x2p,x3p, m, n, w)
+
+  y = (2*w)^-1 +(1/w)*sum;
+end
