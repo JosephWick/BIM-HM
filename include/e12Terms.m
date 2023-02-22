c2baa8537846f29fca71e57c06d886050a71ade3
Joseph Wick <josephwick@outlook.com>
2023-02-22 13:46:11 -0800

    Update e12Terms.m

--- include/e12Terms.m
+++ include/e12Terms.m
@@ -1,8 +1,8 @@
 % terms in the summation for e12
 function y = e12Terms(x2p, x3p, m, n, w)
-  t1 = cosh( (m.*.pi.*(1-x3p)) ./ (w.*sqrt(n)) );
-  t2 = cos( (m.*pi.*x2p) ./ w );
-  t3 = cosh( (m.*pi) ./ (w.*sqrt(n)) );
+  t1 = cosh( (m*pi*(1-x3p)) / (w*sqrt(n)) );
+  t2 = cos( (m*pi*x2p) / w );
+  t3 = cosh( (m*pi) / (w*sqrt(n)) );
 
   y = (t1.*t2)./t3;
 end
