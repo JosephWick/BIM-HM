8712ede78ef28f61c2532348e535b980473590c0
Joseph Wick <josephwick@outlook.com>
2023-03-07 11:22:50 -0800

    update summation limits

--- include/getE12.m
+++ include/getE12.m
@@ -3,7 +3,7 @@ function y = getE12(x2p, x3p, n, w)
   m=1;
   sum = zeros(size(x2p));
 
-  while m <= 3900
+  while m <= 657
     sterm = e12Terms(x2p,x3p, m, n, w);
     sum = sum + sterm;
     m = m+1;
--- include/getE13.m
+++ include/getE13.m
@@ -3,7 +3,7 @@ function y = getE13(x2p, x3p, n, w)
   m=1;
   sum = zeros(size(x2p));
 
-  while m <= 3900
+  while m <= 657
     sterm = e13Terms(x2p,x3p, m, n, w);
     sum = sum + sterm;
     m = m+1;
