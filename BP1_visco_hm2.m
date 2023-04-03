0f39617ac02cf9b089a50ea95c97cc6697e01842
Joseph Wick <josephwick@outlook.com>
2023-04-03 15:57:56 -0700

    Update BP1_visco_hm2.m

--- BP1_visco_hm2.m
+++ BP1_visco_hm2.m
@@ -40,9 +40,9 @@ function r = build()
 
   % Fault Meshes
   ss.probL = 200e3; % left/right; ie yhat
-  ss.probW = 100e3; % up/down; ie zhat
+  ss.probW = 200e3; % up/down; ie zhat
 
-  param1 = 3.1; %3.1 for 100e3 probW, 2.3 for 200e3 probW
+  param1 = 2.3; %3.1 for 100e3 probW, 2.3 for 200e3 probW
 
   ss.lambdaZ = 40e3; % fault depth extent
   ss.M = 1600; % number of fault cells, this is 25m patches
