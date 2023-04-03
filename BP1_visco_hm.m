9e113156bc8e71555a34c7910981557f6464c582
Joseph Wick <josephwick@outlook.com>
2023-04-03 14:29:05 -0700

    Update BP1_visco_hm.m

--- BP1_visco_hm.m
+++ BP1_visco_hm.m
@@ -431,7 +431,9 @@ function out = run(b)
   tic
   % Solve the system
   options=odeset('Refine',1,'RelTol',3e-7,'InitialStep',1e-3,'MaxStep',3e6);
-  [t,Y]=ode45_2(yp,[0 1*3.15e7],Y0,options);
+  tmax = 1*3.15e7;
+  disp(tmax)
+  [t,Y]=ode45_2(yp,[0 tmax],Y0,options);
   disp('done solving.')
   toc
   %%
