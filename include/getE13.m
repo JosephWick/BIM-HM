3cb088026163592bbef1eee6c9b21f287f62ff94
Joseph Wick <josephwick@outlook.com>
2023-04-11 18:18:46 -0700

    Update getE13.m

--- include/getE13.m
+++ include/getE13.m
@@ -3,7 +3,7 @@ function y = getE13(x2p, x3p, n, w)
   m=1;
   sum = zeros(size(x2p));
 
-  while m <= 1
+  while m <= 3936
     sterm = e13Terms(x2p,x3p, m, n, w);
     sum = sum + sterm;
     m = m+1;
