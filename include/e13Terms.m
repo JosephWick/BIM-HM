193507a498b1a8b28b4f1c57f316e6cc6397b9d3
Joseph Wick <josephwick@outlook.com>
2023-04-11 18:20:25 -0700

    .

--- include/e13Terms.m
+++ include/e13Terms.m
@@ -1,7 +1,7 @@
 % terms in the summation for e13
 function y = e13Terms(x2p, x3p, m, n, w)
   t1 = sinh( (m*pi*(1-x3p)) / (w*sqrt(n)) );
-  t2 = sin( (m*pi*x2p) / w );
+  t2 = sin( (m*pi*x2p) / w*sqrt(n) );
   t3 = cosh( (m*pi) / (w*sqrt(n)) );
 
   y = (t1.*t2)./t3;
